22011e3ef927bb2470e5a1547bf2d419b3c08720
jang <dragoalie@gmail.com>
2019-05-28 15:12:11 +0900

    Implement Queue with a single parameter

--- src/main/scala/Queue.scala
+++ src/main/scala/Queue.scala
@@ -4,27 +4,29 @@ trait Queue[T] {
 
   def enQueue(t: T): Queue[T]
 
-  // Removes the element at the beginning of the immutable queue, and returns the new queue.
-  def deQueue(): Queue[T]
+  // Removes the element at the beginning of the immutable queue and returns the new queue.
+  def deQueue: Queue[T]
 
   def head: Option[T]
 
 }
 
-case class MyQueue[T](queue: List[T]) extends Queue[T] {
+case class QueueImpl[T](queue: List[T]) extends Queue[T] {
 
   override def isEmpty: Boolean = queue.isEmpty
 
-  override def enQueue(t: T): Queue[T] = ???
+  override def enQueue(t: T): Queue[T] = QueueImpl(queue ::: List(t))
 
-  override def deQueue(): Queue[T] = ???
+  override def deQueue: Queue[T] = QueueImpl(queue.tail)
 
-  override def head: Option[T] = ???
+  override def head: Option[T] = queue.headOption
 
 }
 
 object Queue {
 
-  def empty[T]: Queue[T] = ???
+  def apply[T](ts: T*): Queue[T] = QueueImpl[T](ts.toList)
+
+  def empty[T]: Queue[T] = QueueImpl(Nil)
 
 }
--- src/test/scala/QueueSpec.scala
+++ src/test/scala/QueueSpec.scala
@@ -5,13 +5,45 @@ class QueueSpec extends FunSpec with Matchers {
   describe("isEmpty") {
 
     it("should return true if queue is empty") {
-      val queue = MyQueue(Nil)
+      val queue = Queue()
       assert(queue.isEmpty)
     }
 
     it("should return false if queue is not empty") {
-      val queue = MyQueue(List(1, 3, 5, 7, 11))
+      val queue = Queue(2, 3, 5)
       assert(!queue.isEmpty)
+    }
+
+  }
+
+  describe("enQueue") {
+
+    it("should return a new Queue that added an element on last") {
+      val expected = Queue(2, 3, 5, 7, 9)
+      val actual = Queue(2, 3, 5, 7).enQueue(9)
+      assert(expected === actual)
+    }
+
+  }
+
+  describe("deQueue") {
+
+    it("should return a new Queue that removed an element at the beginning") {
+      val expected = Queue(3, 5, 7, 9)
+      val actual = Queue(2, 3, 5, 7, 9).deQueue
+      assert(expected === actual)
+    }
+
+  }
+
+  describe("head") {
+
+    it("should return a front optional value if exist") {
+      assert(Some(2) === Queue(2, 3, 5, 7).head)
+    }
+
+    it("should return a none if queue is empty") {
+      assert(None === Queue().head)
     }
 
   }
